fe5fc527e7be46a2b66d3158e223b17b459d8c97
Omkar Patil <omkarpatil9935@gmail.com>
2024-12-24 17:11:26 +0000

    conversion of datatype

---
+++ 01_Basic/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = 33
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)      //  <-- conversion in number
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN 
+// true => 1; false => 0
+// null => 0
+
+// let isLoggedIn = 1
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)   // <-- conversion in boolean 
+// console.log(booleanIsLoggedIn);
+
+// 1 => true
+// "" => false 
+// "omkar" => false
+
+
+let strNumber = 33
+
+let stringNumber = String(strNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
