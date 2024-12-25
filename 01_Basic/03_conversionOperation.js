b65f8bbbbac21237afebddec73919605c46a928b
Omkar Patil <omkarpatil9935@gmail.com>
2024-12-25 17:58:54 +0000

    operation

--- 01_Basic/03_conversionOperation.js
+++ 01_Basic/03_conversionOperation.js
@@ -25,5 +25,63 @@ let score = 33
 let strNumber = 33
 
 let stringNumber = String(strNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ****** Operation ******
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 3);
+// console.log(2 / 2);
+// console.log(2 % 3);
+
+
+let str1 = "hello"
+let str2 = " omkar"
+let str3 = str1 + str2 
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4 ) * 5 % 3);    // --> perfer not to use diffcult (readability)
+
+
+// console.log(+true);   // 1
+// console.log(+"");     // 0
+
+
+let num1, num2, num3 
+
+num1 = num2 = num3 = 2 + 2
+
+
+let gameCounter = 100
+gameCounter++;
+// console.log(gameCounter);       // 101
+
+// *** PostFix ***  [return the value before incrementing]
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// output: "x:4, y:3"
+
+
+// *** PreFix *** [return the value after incrementing]
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+//  output: "a:4, b:4"
+
