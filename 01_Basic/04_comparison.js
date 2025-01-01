c7a58e2cd51891ed109a366f511b42a67f5fe144
Omkar Patil <omkarpatil9935@gmail.com>
2025-01-01 14:15:40 +0000

    comparison

---
+++ 01_Basic/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log( 2 > 1);
+// console.log( 2 >= 1);
+// console.log( 2 < 1);
+// console.log( 2 == 1);
+// console.log( 2 != 1);
+
+// console.log("2" > 1);   //true     --> here it convert string value into number for comparison
+// console.log("02" > 1);  //true
+
+
+// console.log(null > 0);   //false       --> comparison convert null vlaue to 0;
+// console.log(null == 0);  //false       --> equality only checks    
+// console.log(null >= 0);  //true
+
+// console.log(undefined == 0);  //false
+// console.log(undefined > 0);  //false
+// console.log(undefined < 0);  //false
+
+
+// ===    --> "strict check operator" [checks both value and its datatype]
+console.log("2" === 2);  //false
