82517ceb3c1e24fa5bfe85c197443a73b8d1761f
Omkar Patil <omkarpatil9935@gmail.com>
2025-01-17 15:42:47 +0000

    function with object & array

--- 03_Basic/01_functions.js
+++ 03_Basic/01_functions.js
@@ -22,7 +22,7 @@ function addTwoNumbers (number1, number2){
 
 const result = addTwoNumbers(5, 9)
 
-console.log("Result:", result);
+// console.log("Result:", result);
 
 
 function userLoginMessage(username = "user") {
@@ -33,4 +33,37 @@ function userLoginMessage(username = "user") {
     return `${username} just logged in`
 }
 
-console.log(userLoginMessage("OMKAR"));
+// console.log(userLoginMessage("OMKAR"));
+
+
+function calculateCartPrice(val1, val2, ...num1) {          // (...) => rest operator
+    return num1
+}
+
+console.log(calculateCartPrice(200, 400, 500, 100)) // [ 500, 100 ] => bcz val1 = 200, val2 = 400
+
+
+const user = {
+    userName: "OMKAR",
+    price: 599
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.userName} and price is ${anyObject.price}`)
+}
+
+// handleObject(user)
+handleObject({
+    userName: "OM",
+    price: 499
+})
+
+
+const newArray = [200, 300, 600, 100]
+
+function returnSecondValue(anyArray){
+    return anyArray[1]
+}
+
+console.log(returnSecondValue(newArray));
+console.log(returnSecondValue([2, 54, 123, 73]));
