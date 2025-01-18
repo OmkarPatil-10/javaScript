1d5b822bcdcd907dc17b3bff32274132c74d251c
Omkar Patil <omkarpatil9935@gmail.com>
2025-01-18 17:49:39 +0000

    scope level  & hoisting

--- 03_Basic/02_scope.js
+++ 03_Basic/02_scope.js
@@ -10,10 +10,58 @@ if (true) {
     let a = 10 
     const b = 20
     // var c = 30
-    console.log("INNER: ", a); 
+    // console.log("INNER: ", a); 
     
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+// +++++ Nested Scope +++++++
+
+function one(){
+    const username= "Omkar"
+
+    function two (){
+        const website = "youtube"
+        // console.log(username);
+    }
+    // console.log(website)  <= not accesible
+
+    two()
+}
+
+one()
+
+
+if (true){
+    const username = "OMKAR"
+    if (username === "OMKAR") {
+        const website = " youtube"
+        // console.log(username + website)
+    }
+    // console.log(website) <= not accesible
+}
+
+// console.log(username) <= not accesible
+
+
+
+//  +++++++++ Hoisting concept ++++++++++
+
+console.log(addOne(5))  // <= this can be call anywhere i.e before & after the function made 
+
+function addOne (num){
+    return num + 1
+}
+
+
+
+// it is function but it called as "Expression"
+const addTwo = function(num) {
+    return num + 2
+}
+
+console.log(addTwo(5)) // <= this called after the function is made
