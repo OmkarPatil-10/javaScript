efda22f66d469380a04f513254391be399c940b6
Omkar Patil <omkarpatil9935@gmail.com>
2025-01-18 17:13:21 +0000

    scope

---
+++ 03_Basic/02_scope.js
@@ -0,0 +1,19 @@
+// global scope
+
+//{} <= scope
+var c = 300
+let a = 300
+
+
+//block scope 
+if (true) {
+    let a = 10 
+    const b = 20
+    // var c = 30
+    console.log("INNER: ", a); 
+    
+}
+
+console.log(a);
+// console.log(b);
+// console.log(c);
