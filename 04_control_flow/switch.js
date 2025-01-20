76270a0cb3f3ed2585d4b89992e68fe677d8ec98
Omkar Patil <omkarpatil9935@gmail.com>
2025-01-20 15:10:42 +0000

    SwitchCase

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,58 @@
+// Switch Conditon
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month = 3
+
+// switch (month) {
+//     case 1:
+//         console.log("January")
+//         break;
+//     case 2:
+//         console.log("Febuary")
+//         break;
+//     case 3:
+//         console.log("March")
+//         break;
+//     case 4:
+//         console.log("April")
+//         break;
+
+//     default:
+//         console.log("Enter Valid Month No. ");
+        
+//         break;
+// }
+
+
+const monthName = "Feb"
+
+// switch (monthName) {
+//     case "Jan":
+//         console.log("January")
+//         break;
+//     case "Feb":
+//         console.log("Febuary")
+//         break;
+//     case "Mar":
+//         console.log("March")
+//         break;
+//     case "April":
+//         console.log("April")
+//         break;
+
+//     default:
+//         console.log("Enter Valid Month Name ");
+        
+//         break;
+// }
+
+
