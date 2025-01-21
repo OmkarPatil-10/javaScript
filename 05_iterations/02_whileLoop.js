eb649aecafa37bd285f721385e58843010b781fa
Omkar Patil <omkarpatil9935@gmail.com>
2025-01-21 18:11:35 +0000

    while Loop

---
+++ 05_iterations/02_whileLoop.js
@@ -0,0 +1,27 @@
+//  1. while loop
+
+let i = 0
+while (i <= 10) {
+    // console.log(`Value of i is ${i}`);
+    i = i + 2
+}
+
+
+// array 
+
+myArray = ["spiderMan", "ironMan", "hulk", "thor"]
+
+let arr = 0
+while (arr < myArray.length ) {
+    // console.log(`Value is ${myArray[arr]}`);
+    arr++
+}
+
+
+
+// 2. do While Loop
+
+let score = 11
+do {
+    console.log(`Value is ${score}`); 
+} while (score <= 10);
