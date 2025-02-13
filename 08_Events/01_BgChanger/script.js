ad87b50544c2387e8667d7e51e5c8a0493b1319e
OmkarPatil-10 <omkarpatil9935@gamil.com>
2025-02-13 22:22:53 +0530

    Project 1

---
+++ 08_Events/01_BgChanger/script.js
@@ -0,0 +1,36 @@
+//generate random color 
+// console.log(Math.floor(Math.random() * 16)
+
+const randomColor = function() {
+  const hex = '0123456789ABCDEF';
+  let color = '#';
+
+  for (let i = 0; i < 6; i++) { 
+    color += hex[Math.floor(Math.random() * 16)]; // Append a random hex character
+  }
+  return color; 
+};
+
+let randomBgColor;
+
+const startChangingColor = function(){
+  changeBgColor = function (){
+    document.body.style.backgroundColor = randomColor();
+    console.log(randomColor());
+  }
+  // randomBgColor = setInterval(changeBgColor, 1000)
+
+  if(!randomBgColor){
+    randomBgColor = setInterval(changeBgColor, 1000)
+  }
+}
+
+const stopChangingColor = function(){
+  clearInterval(randomBgColor)
+  randomBgColor = null;
+}
+
+document.querySelector('#start').addEventListener('click', 
+startChangingColor)
+
+document.querySelector('#stop').addEventListener('click', stopChangingColor)
