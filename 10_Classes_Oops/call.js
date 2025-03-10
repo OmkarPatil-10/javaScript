4b9a54aa9f35254a242954e4c1d9ad1874ca3b03
Omkar Patil <omkarpatil9935@gmail.com>
2025-03-10 16:15:09 +0000

    Call & this

---
+++ 10_Classes_Oops/call.js
@@ -0,0 +1,18 @@
+function setUsername (username){
+    //complex DB calls
+    this.username = username
+    console.log("called");
+    
+}
+
+function createUser(username, email, password){
+    setUsername.call(this, username)
+    this.email = email
+    this.password = password
+}
+
+const omkar = new createUser("omkar", "omkar@gmail.com", "123")
+console.log(omkar);
+
+
+//call => it passes our current context to other 
